5e3cf027e1ba297cd4f691aa6f2073f5d156b502
KHP53 <khp4121@gmail.com>
2024-05-18 08:49:44 -0230

    added inital auth module

---
+++ lib/modules/auth/auth_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon_user_app/modules/auth/auth_viewmodel.dart';
+import 'package:hackathon_user_app/modules/auth/widgets/login_body.dart';
+import 'package:hackathon_user_app/modules/view.dart';
+
+class AuthView extends StatelessWidget {
+  const AuthView({super.key, required this.isLogin});
+  final bool isLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BaseView(
+        viewmodel: AuthViewmodel(),
+        builder: (_, viewmodel, __) {
+          return isLogin
+              ? LoginBody(
+                  viewmodel: viewmodel,
+                )
+              : Container();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/modules/auth/widgets/login_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon_user_app/modules/auth/auth_viewmodel.dart';
+
+class LoginBody extends StatelessWidget {
+  const LoginBody({super.key, required this.viewmodel});
+  final AuthViewmodel viewmodel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
